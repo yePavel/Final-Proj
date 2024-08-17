c6cb177cc2630d9c54e504650c10edc020398f00
pavel <pavel.yv@gmail.com>
2024-08-17 11:49:54 +0300

    Add main home page

--- PlanIt/src/RootCmp.jsx
+++ PlanIt/src/RootCmp.jsx
@@ -1,18 +1,20 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { AppHeader } from "./cmps/AppHeader.jsx";
+import { HomePage } from "./pages/HomePage.jsx";
 
 
 export function RootCmp() {
 
     return (
-            <Router>
-                <section className="app">
-                    <AppHeader />
-                    <main className="main-layout">
-                        {/* <Routes>
-                        </Routes> */}
-                    </main>
-                </section>
-            </Router>
+        <Router>
+            <section className="app">
+                <AppHeader />
+                <main className="main-layout">
+                    <Routes>
+                        <Route path="" element={<HomePage />} />
+                    </Routes>
+                </main>
+            </section>
+        </Router>
     )
 }
