e481b092da2b930ddc66041416e22735002ee42f
pavel <pavel.yv@gmail.com>
2024-08-17 15:07:08 +0300

    Add navigation to home page from logo

--- PlanIt/src/cmps/AppHeader.jsx
+++ PlanIt/src/cmps/AppHeader.jsx
@@ -23,11 +23,11 @@ export function AppHeader() {
                     <svg className="hover-icon" width="24" height="24" viewBox="0 0 24 24">
                         <path fillRule="evenodd" clipRule="evenodd" d="M3 5C3 3.89543 3.89543 3 5 3H19C20.1046 3 21 3.89543 21 5V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V5ZM5 6C5 5.44772 5.44772 5 6 5H10C10.5523 5 11 5.44772 11 6V16C11 16.5523 10.5523 17 10 17H6C5.44772 17 5 16.5523 5 16V6ZM14 5C13.4477 5 13 5.44772 13 6V12C13 12.5523 13.4477 13 14 13H18C18.5523 13 19 12.5523 19 12V6C19 5.44772 18.5523 5 18 5H14Z" fill="currentColor" />
                     </svg>
+                    <a href="/" className="logo">PlanIt</a>
                 </div>
-                <div className="logo">PlanIt</div>
                 <nav className="nav">
                     <div className="nav-item" onClick={handleWorkspaceClick}>
-                        Workspaces 
+                        Workspaces
                         <svg width="16" height="16" viewBox="0 0 24 24">
                             <path d="M11.2929 16.7071L4.22185 9.63606C3.83132 9.24554 3.83132 8.61237 4.22185 8.22185C4.61237 7.83133 5.24554 7.83133 5.63606 8.22185L12 14.5858L18.364 8.22185C18.7545 7.83132 19.3877 7.83132 19.7782 8.22185C20.1687 8.61237 20.1687 9.24554 19.7782 9.63606L12.7071 16.7071C12.3166 17.0977 11.6834 17.0977 11.2929 16.7071Z" fill="currentColor" />
                         </svg>
--- PlanIt/src/pages/HomePage.jsx
+++ PlanIt/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export function HomePage() {
+
     return (
         <section className='homepage'>
 
