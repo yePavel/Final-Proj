e468d2177a2e71002de1e3659cf9bf7cab77aaac
pavel <pavel.yv@gmail.com>
2024-08-17 16:03:18 +0300

    Add boards page

--- PlanIt/src/RootCmp.jsx
+++ PlanIt/src/RootCmp.jsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage.jsx";
 import { AppHeader } from "./cmps/AppHeader.jsx";
 import { BoardList } from "./cmps/BoardList.jsx";
+import { Boards } from "./pages/Boards.jsx";
 
 
 export function RootCmp() {
@@ -13,8 +14,9 @@ export function RootCmp() {
                 <main className="main-layout">
                     <Routes>
                         <Route path="" element={<HomePage />} />
+                        <Route path="/boards" element={<Boards />} />
                         <Route path="/boards/:boardId" element={<BoardList />} />
-                        </Routes>
+                    </Routes>
                 </main>
             </section>
         </Router>
---
+++ PlanIt/src/pages/Boards.jsx
@@ -0,0 +1,34 @@
+
+export function Boards() {
+    return <section className="main-boards-container">
+        <nav className="boards-manu">
+            <div className="upper-manu">
+                <ul>
+                    <li className="active">Boards</li>
+                    <li>Templates</li>
+                    <li>Home</li>
+                </ul>
+            </div>
+            <div className="lower-manu">
+                <ul>
+                    <li>Gatting started</li>
+                    <li>Boards</li>
+                    <li>Collections</li>
+                    <li>Highlights</li>
+                    <li>Views</li>
+                    <li>Members</li>
+                    <li>Settings</li>
+                    <li>Billing</li>
+                </ul>
+            </div>
+        </nav>
+        <div className="board-list-container">
+            <div className="starred-boards">
+                <h3>⭐ Starred boards</h3>
+            </div>
+            <div className="user-boards">
+                <h3>Your workspaces</h3>
+            </div>
+        </div>
+    </section>
+}
