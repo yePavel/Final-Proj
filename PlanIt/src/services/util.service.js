97128c81d644ceccca470c20a95a4c397238cf6a
DANIELYACOVI <dannyyacovi6@gmail.com>
2024-08-18 21:36:27 +0300

    Connect board service

--- PlanIt/src/services/util.service.js
+++ PlanIt/src/services/util.service.js
@@ -1,13 +1,4 @@
-export const utilService = {
-    makeId,
-    makeLorem,
-    getRandomIntInclusive,
-    loadFromStorage,
-    saveToStorage,
-    animateCSS
-}
-
-function makeId(length = 6) {
+export function makeId(length = 6) {
     var txt = ''
     var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
@@ -18,43 +9,45 @@ function makeId(length = 6) {
     return txt
 }
 
-function makeLorem(size = 100) {
-    const words = ['The sky', 'above', 'the port', 'was', 'the color' ,'of nature', 'tuned', 'to', 'a live channel', 'All', 'this happened', 'more or less', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', 'a pleasure', 'to', 'burn']
+export function makeLorem(size = 100) {
+    var words = ['The sky', 'above', 'the port', 'was', 'the color of television', 'tuned', 'to', 'a dead channel', '.', 'All', 'this happened', 'more or less', '.', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', '.', 'It', 'was', 'a pleasure', 'to', 'burn']
     var txt = ''
     while (size > 0) {
         size--
-        txt += words[Math.floor(Math.random() * words.length)]
-        if (size >= 1 ) txt += ' '
+        txt += words[Math.floor(Math.random() * words.length)] + ' '
     }
     return txt
 }
 
-function getRandomIntInclusive(min, max) {
+export function getRandomIntInclusive(min, max) {
     min = Math.ceil(min)
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive 
 }
 
-function saveToStorage(key, value) {
+
+export function randomPastTime() {
+    const HOUR = 1000 * 60 * 60
+    const DAY = 1000 * 60 * 60 * 24
+    const WEEK = 1000 * 60 * 60 * 24 * 7
+
+    const pastTime = getRandomIntInclusive(HOUR, WEEK)
+    return Date.now() - pastTime
+}
+
+export function debounce(func, timeout = 300) {
+    let timer
+    return (...args) => {
+        clearTimeout(timer)
+        timer = setTimeout(() => { func.apply(this, args) }, timeout)
+    }
+}
+
+export function saveToStorage(key, value) {
     localStorage.setItem(key, JSON.stringify(value))
 }
 
-function loadFromStorage(key) {
+export function loadFromStorage(key) {
     const data = localStorage.getItem(key)
     return (data) ? JSON.parse(data) : undefined
-}
-
-function animateCSS(el, animation='bounce') {
-    const prefix = 'animate__'
-    return new Promise((resolve, reject) => {
-        const animationName = `${prefix}${animation}`
-        el.classList.add(`${prefix}animated`, animationName)
-        function handleAnimationEnd(event) {
-            event.stopPropagation()
-            el.classList.remove(`${prefix}animated`, animationName)
-            resolve('Animation ended')
-        }
-
-        el.addEventListener('animationend', handleAnimationEnd, { once: true })
-    })
 }
