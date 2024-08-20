67606bf661addacb5ef062d318a8a4a3055faf05
pavel <pavel.yv@gmail.com>
2024-08-20 10:09:34 +0300

    Improve boardIndex cmp ul style

--- PlanIt/src/pages/BoardIndex.jsx
+++ PlanIt/src/pages/BoardIndex.jsx
@@ -1,10 +1,15 @@
 
 export function BoardIndex() {
+
+
     return <section className="main-boards-container">
         <nav className="boards-manu">
             <div className="upper-manu">
                 <ul>
-                    <li className="active">Boards</li>
+                    <li className="active">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" role="presentation" focusable="false" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M3 5C3 3.89543 3.89543 3 5 3H19C20.1046 3 21 3.89543 21 5V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V5ZM5 6C5 5.44772 5.44772 5 6 5H10C10.5523 5 11 5.44772 11 6V16C11 16.5523 10.5523 17 10 17H6C5.44772 17 5 16.5523 5 16V6ZM14 5C13.4477 5 13 5.44772 13 6V12C13 12.5523 13.4477 13 14 13H18C18.5523 13 19 12.5523 19 12V6C19 5.44772 18.5523 5 18 5H14Z" fill="#000000">
+                        </path>
+                        </svg> Boards</li>
                     <li>Templates</li>
                     <li>Home</li>
                 </ul>
@@ -12,7 +17,11 @@ export function BoardIndex() {
             <div className="lower-manu">
                 <ul>
                     <li>Gatting started</li>
-                    <li>Boards</li>
+                    <li>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" role="presentation" focusable="false" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M3 5C3 3.89543 3.89543 3 5 3H19C20.1046 3 21 3.89543 21 5V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V5ZM5 6C5 5.44772 5.44772 5 6 5H10C10.5523 5 11 5.44772 11 6V16C11 16.5523 10.5523 17 10 17H6C5.44772 17 5 16.5523 5 16V6ZM14 5C13.4477 5 13 5.44772 13 6V12C13 12.5523 13.4477 13 14 13H18C18.5523 13 19 12.5523 19 12V6C19 5.44772 18.5523 5 18 5H14Z" fill="#000000">
+                        </path>
+                        </svg> Boards
+                    </li>
                     <li>Collections</li>
                     <li>Highlights</li>
                     <li>Views</li>
@@ -31,7 +40,6 @@ export function BoardIndex() {
                     </svg>
                     <h3> Starred boards</h3>
                 </div>
-
                 <ul className="starred-boards-list">
                     <li className="board-item"><div>Final proj</div></li>
                     <li className="board-item"><div>Project 1</div></li>
@@ -40,7 +48,9 @@ export function BoardIndex() {
                 </ul>
             </div>
             <div className="user-boards">
-                <h3>👷 Your workspaces</h3>
+                <div className="starred-tittle">
+                    <h3> Your workspaces</h3>
+                </div>
                 <ul className="user-boards-list">
                     <li className="board-item"><div>workSpace 1</div></li>
                     <li className="board-item"><div>workSpace 2</div></li>
--- PlanIt/src/services/board/board.service.local.js
+++ PlanIt/src/services/board/board.service.local.js
@@ -6,275 +6,275 @@ import { userService } from '../user'
 const STORAGE_KEY = 'board'
 
 export const boardService = {
-    query,
-    getById,
-    save,
-    remove,
-    addBoardMsg
+  query,
+  getById,
+  save,
+  remove,
+  addBoardMsg
 }
 window.cs = boardService
 
 const data = [
-    {
-      title: "Final Proj",
-      isStarred: false,
-      archivedAt: 1589983468418,
-      createdBy: {
-        _id: "u101",
-        fullname: "Daniel Yacovi",
+  {
+    title: "Final Proj",
+    isStarred: false,
+    archivedAt: 1589983468418,
+    createdBy: {
+      _id: "u101",
+      fullname: "Daniel Yacovi",
+      imgUrl: "",
+    },
+    style: {
+      backgroundImage: "",
+    },
+    labels: [
+      {
+        id: "l101",
+        title: "Done",
+        color: "#61bd4f",
+      },
+      {
+        id: "l102",
+        title: "Progress",
+        color: "#9f8fef",
+      },
+    ],
+    members: [
+      {
+        _id: "u102",
+        fullname: "Sean Mamistalov",
         imgUrl: "",
       },
-      style: {
-        backgroundImage: "",
+      {
+        _id: "u103",
+        fullname: "Pavel Yelsukov",
+        imgUrl: "",
       },
-      labels: [
-        {
-          id: "l101",
-          title: "Done",
-          color: "#61bd4f",
-        },
-        {
-          id: "l102",
-          title: "Progress",
-          color: "#9f8fef",
-        },
-      ],
-      members: [
-        {
-          _id: "u102",
-          fullname: "Sean Mamistalov",
+    ],
+    groups: [
+      {
+        id: "g101",
+        title: "Backlog Server",
+        archivedAt: 1589983468418,
+        tasks: [
+          {
+            id: "c101",
+            title: "Replace logo",
+          },
+          {
+            id: "c102",
+            title: "Add Samples",
+          },
+        ],
+        style: {},
+      },
+      {
+        id: "g102",
+        title: "Backlog Client",
+        tasks: [
+          {
+            id: "c103",
+            title: "Do that",
+            archivedAt: 1589983468418,
+          },
+          {
+            id: "c104",
+            title: "Help me",
+            status: "inProgress",
+            priority: "high",
+            dueDate: "2024-09-24",
+            description: "description",
+            comments: [
+              {
+                id: "ZdPnm",
+                title: "also @yaronb please CR this",
+                createdAt: 1590999817436,
+                byMember: {
+                  _id: "u101",
+                  fullname: "Daniel Yacovi",
+                  imgUrl: "",
+                },
+              },
+            ],
+            checklists: [
+              {
+                id: "YEhmF",
+                title: "Checklist",
+                todos: [
+                  {
+                    id: "212jX",
+                    title: "To Do 1",
+                    isDone: false,
+                  },
+                ],
+              },
+            ],
+            memberIds: ["u102"],
+            labelIds: ["l101", "l102"],
+            byMember: {
+              _id: "u103",
+              fullname: "Pavel Yelsukov",
+              imgUrl: "",
+            },
+            style: {
+              backgroundColor: "#26de81",
+            },
+          },
+        ],
+        style: {},
+      },
+      {
+        id: "g103",
+        title: "In development",
+        archivedAt: 1589983468418,
+        tasks: [
+          {
+            id: "c105",
+            title: "Main home page",
+          },
+          {
+            id: "c106",
+            title: "Footer",
+          },
+        ],
+        style: {},
+      },
+      {
+        id: "g104",
+        title: "Done",
+        archivedAt: 1589983468418,
+        tasks: [
+          {
+            id: "c107",
+            title: "Login-cmp",
+          },
+          {
+            id: "c108",
+            title: "SCSS vars",
+          },
+        ],
+        style: {},
+      },
+      {
+        id: "g105",
+        title: "QA",
+        tasks: [
+          {
+            id: "c108",
+            title: "Conduct User Testing",
+            status: "pending",
+            priority: "high",
+          },
+          {
+            id: "c109",
+            title: "Bug Fixes",
+            status: "pending",
+            priority: "high",
+          },
+        ],
+        style: {},
+      },
+      {
+        id: "g106",
+        title: "Ready for Production",
+        tasks: [
+          {
+            id: "c110",
+            title: "Finalize Documentation",
+            status: "pending",
+          },
+          {
+            id: "c114",
+            title: "Deploy to Staging",
+            status: "pending",
+            priority: "high",
+          },
+        ],
+        style: {},
+      },
+    ],
+    activities: [
+      {
+        id: "a101",
+        title: "Changed Color",
+        createdAt: 154514,
+        byMember: {
+          _id: "u101",
+          fullname: "Daniel Yacovi",
           imgUrl: "",
         },
-        {
-          _id: "u103",
-          fullname: "Pavel Yelsukov",
-          imgUrl: "",
-        },
-      ],
-      groups: [
-        {
+        group: {
           id: "g101",
           title: "Backlog Server",
-          archivedAt: 1589983468418,
-          tasks: [
-            {
-              id: "c101",
-              title: "Replace logo",
-            },
-            {
-              id: "c102",
-              title: "Add Samples",
-            },
-          ],
-          style: {},
         },
-        {
-          id: "g102",
-          title: "Backlog Client",
-          tasks: [
-            {
-              id: "c103",
-              title: "Do that",
-              archivedAt: 1589983468418,
-            },
-            {
-              id: "c104",
-              title: "Help me",
-              status: "inProgress",
-              priority: "high",
-              dueDate: "2024-09-24",
-              description: "description",
-              comments: [
-                {
-                  id: "ZdPnm",
-                  title: "also @yaronb please CR this",
-                  createdAt: 1590999817436,
-                  byMember: {
-                    _id: "u101",
-                    fullname: "Daniel Yacovi",
-                    imgUrl: "",
-                  },
-                },
-              ],
-              checklists: [
-                {
-                  id: "YEhmF",
-                  title: "Checklist",
-                  todos: [
-                    {
-                      id: "212jX",
-                      title: "To Do 1",
-                      isDone: false,
-                    },
-                  ],
-                },
-              ],
-              memberIds: ["u102"],
-              labelIds: ["l101", "l102"],
-              byMember: {
-                _id: "u103",
-                fullname: "Pavel Yelsukov",
-                imgUrl: "",
-              },
-              style: {
-                backgroundColor: "#26de81",
-              },
-            },
-          ],
-          style: {},
+        task: {
+          id: "c101",
+          title: "Replace Logo",
         },
-        {
-          id: "g103",
-          title: "In development",
-          archivedAt: 1589983468418,
-          tasks: [
-            {
-              id: "c105",
-              title: "Main home page",
-            },
-            {
-              id: "c106",
-              title: "Footer",
-            },
-          ],
-          style: {},
-        },
-        {
-          id: "g104",
-          title: "Done",
-          archivedAt: 1589983468418,
-          tasks: [
-            {
-              id: "c107",
-              title: "Login-cmp",
-            },
-            {
-              id: "c108",
-              title: "SCSS vars",
-            },
-          ],
-          style: {},
-        },
-        {
-          id: "g105",
-          title: "QA",
-          tasks: [
-            {
-              id: "c108",
-              title: "Conduct User Testing",
-              status: "pending",
-              priority: "high",
-            },
-            {
-              id: "c109",
-              title: "Bug Fixes",
-              status: "pending",
-              priority: "high",
-            },
-          ],
-          style: {},
-        },
-        {
-          id: "g106",
-          title: "Ready for Production",
-          tasks: [
-            {
-              id: "c110",
-              title: "Finalize Documentation",
-              status: "pending",
-            },
-            {
-              id: "c114",
-              title: "Deploy to Staging",
-              status: "pending",
-              priority: "high",
-            },
-          ],
-          style: {},
-        },
-      ],
-      activities: [
-        {
-          id: "a101",
-          title: "Changed Color",
-          createdAt: 154514,
-          byMember: {
-            _id: "u101",
-            fullname: "Daniel Yacovi",
-            imgUrl: "",
-          },
-          group: {
-            id: "g101",
-            title: "Backlog Server",
-          },
-          task: {
-            id: "c101",
-            title: "Replace Logo",
-          },
-        },
-      ],
-      cmpsOrder: ["StatusPicker", "MemberPicker", "DatePicker"],
-    },
-  ];
-  
-  _createBoard()
-  
-  
-  // export const getProjectData = () => {
-  //   const storedData = localStorage.getItem('boards')
-  //   if (storedData) {
-  //     return new Promise(resolve => resolve(JSON.parse(storedData)))
-  //   } else {
-  //     localStorage.setItem('board', JSON.stringify(data))
-  //     return new Promise(resolve => resolve(data))
-  //   }
-  // }
+      },
+    ],
+    cmpsOrder: ["StatusPicker", "MemberPicker", "DatePicker"],
+  },
+];
+
+_createBoard()
+
+
+// export const getProjectData = () => {
+//   const storedData = localStorage.getItem('boards')
+//   if (storedData) {
+//     return new Promise(resolve => resolve(JSON.parse(storedData)))
+//   } else {
+//     localStorage.setItem('board', JSON.stringify(data))
+//     return new Promise(resolve => resolve(data))
+//   }
+// }
 
 
 async function query(filterBy = { title: '' }) {
-    var boards = await storageService.query(STORAGE_KEY)
-    const { title } = filterBy
+  var boards = await storageService.query(STORAGE_KEY)
+  const { title } = filterBy
 
-    if (title) {
-        const regex = new RegExp(filterBy.title, 'i')
-        boards = boards.filter(board => regex.test(board.group.title)) // || regex.test(board.description)
-    }
-        return boards
+  if (title) {
+    const regex = new RegExp(filterBy.title, 'i')
+    boards = boards.filter(board => regex.test(board.group.title)) // || regex.test(board.description)
+  }
+  return boards
 }
 
 function getById(boardId) {
-    return storageService.get(STORAGE_KEY, boardId)
+  return storageService.get(STORAGE_KEY, boardId)
 }
 
 async function remove(boardId) {
-    // throw new Error('Nope')
-    await storageService.remove(STORAGE_KEY, boardId)
+  // throw new Error('Nope')
+  await storageService.remove(STORAGE_KEY, boardId)
 }
 
 function save(board) {
   if (board._id) {
-      return storageService.put(STORAGE_KEY, board)
+    return storageService.put(STORAGE_KEY, board)
   } else {
 
-      return storageService.post(STORAGE_KEY, board)
+    return storageService.post(STORAGE_KEY, board)
   }
 }
 
 async function addBoardMsg(boardId, txt) {
-    // Later, this is all done by the backend
-    const board = await getById(boardId)
+  // Later, this is all done by the backend
+  const board = await getById(boardId)
 
-    const msg = {
-        id: makeId(),
-        by: userService.getLoggedinUser(),
-        txt
-    }
-    board.msgs.push(msg)
-    await storageService.put(STORAGE_KEY, board)
+  const msg = {
+    id: makeId(),
+    by: userService.getLoggedinUser(),
+    txt
+  }
+  board.msgs.push(msg)
+  await storageService.put(STORAGE_KEY, board)
 
-    return msg
+  return msg
 }
 
-function _createBoard(){
+function _createBoard() {
   saveToStorage(STORAGE_KEY, data)
 }
