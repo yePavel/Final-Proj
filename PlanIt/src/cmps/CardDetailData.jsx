42b1792e9913ab71ca98096b22c5999785932173
DANIELYACOVI <dannyyacovi6@gmail.com>
2024-09-06 17:13:13 +0300

    Fix card details

--- PlanIt/src/cmps/CardDetailData.jsx
+++ PlanIt/src/cmps/CardDetailData.jsx
@@ -12,7 +12,6 @@ export function CardDetailData() {
   function toggleIsWatching() {
     setIsWatching(res => !res)
   }
-  console.log('task:', task)
 
   return (
     <div className="card-detail-data">
--- PlanIt/src/cmps/TaskList.jsx
+++ PlanIt/src/cmps/TaskList.jsx
@@ -1,8 +1,26 @@
 import { Draggable } from "react-beautiful-dnd";
 import { LabelPreview } from "./LabelPreview";
 import { AssignedMember } from "./AssignedMember";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import { loadTask } from "../store/actions/board.actions.js";
+import { CardModal } from "./CardModal.jsx";
 
 export function TaskList({ el, provided }) {
+    const board = useSelector(storeState => storeState.boardModule.board)
+    
+    const [selectedTask, setSelectedTask] = useState(null);
+    const [currGroup, setGroup] = useState(null);
+
+    async function onTaskClick(boardId, group, taskId) {
+        await loadTask(boardId, group.id, taskId);
+        setSelectedTask(taskId);
+        setGroup(group);
+    }
+
+    function onCloseModal() {
+        setSelectedTask(null);
+    }
 
     return <div className="tasks">
         {el.tasks.map((item, index) => (
@@ -16,10 +34,16 @@ export function TaskList({ el, provided }) {
                         ref={provided.innerRef}
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
+                        onClick={() => onTaskClick(board._id, el, item.id)}
                     >
                         <LabelPreview labels={item.labels} />
                         <p className="task-title">{item.title}</p>
                         <AssignedMember members={item.members} />
+
+                        {selectedTask && (
+                            <CardModal group={currGroup} onClose={onCloseModal} />
+                        )}
+
                     </div>
                 )}
             </Draggable>
