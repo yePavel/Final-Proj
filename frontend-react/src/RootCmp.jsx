44a7142390d2db360885717aa5744da8d0067d64
Sean <mamistalovsean@gmail.com>
2024-09-14 06:01:01 +0300

    Update rootcmp

--- frontend-react/src/RootCmp.jsx
+++ frontend-react/src/RootCmp.jsx
@@ -1,25 +1,26 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-
 import { HomePage } from "./pages/HomePage.jsx";
 import { AppHeader } from "./cmps/AppHeader.jsx";
 import { BoardDetails } from "./pages/BoardDetails.jsx";
 import { BoardIndex } from "./pages/BoardIndex.jsx";
 import { DragAndDrop } from "./cmps/DragAndDrop.jsx";
-
+import { SignUp } from "./cmps/SignUp.jsx";
+import { Auth } from "./pages/Auth.jsx";
 
 export function RootCmp() {
-
-    return (
-        <div className={'app'}>
-            <AppHeader />
-            <main className="main-layout">
-                <Routes>
-                    <Route path="" element={<HomePage />} />
-                    <Route path="/boards" element={<BoardIndex />} />
-                    <Route path="/boards/:boardId" element={<BoardDetails />} />
-                    <Route path="/boards/drag" element={<DragAndDrop />} />
-                </Routes>
-            </main>
-        </div>
-    )
+  return (
+    <div className={"app"}>
+      <AppHeader />
+      <main className="main-layout">
+        <Routes>
+          <Route path="/signup" element={<SignUp isSignup={true} />} />
+          <Route path="/login" element={<SignUp isSignup={false} />} />
+          <Route path="" element={<HomePage />} />
+          <Route path="/boards" element={<BoardIndex />} />
+          <Route path="/boards/:boardId" element={<BoardDetails />} />
+          <Route path="/boards/drag" element={<DragAndDrop />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
